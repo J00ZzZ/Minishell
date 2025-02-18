87ace33a503436ed8a58f0a3b1ee96b5e2a81208
harleynghx <harleynghx.edu@gmail.com>
2025-02-19 07:04:54 +0800

    FINAL SPLIT

---
+++ main.c
@@ -0,0 +1,31 @@
+#include "minishell.h"
+
+int	main(void)
+{
+	char		*input;
+	t_history	*history;
+
+	history = NULL;
+	while (1)
+	{
+		ft_printf("minishell> ");
+		input = read_input();
+		if (!input)
+			break ;
+		if (ft_strncmp(input, "history", 7) == 0)
+			print_history(history);
+		else if (ft_strncmp(input, "exit", 4) == 0)
+		{
+			free(input);
+			break ;
+		}
+		else if (ft_strlen(input) > 0)
+		{
+			add_to_history(&history, input);
+			execute_command(input);
+		}
+		free(input);
+	}
+	free_history(history);
+	return (0);
+}
--- minishell.h
+++ minishell.h
@@ -1,37 +1,7 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   minishell.h                                        :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: liyu-her <liyu-her@student.42.kl>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/02/19 04:41:56 by liyu-her          #+#    #+#             */
-/*   Updated: 2025/02/19 04:42:06 by liyu-her         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
-# include "libft/libft.h" // Include the libft header
-# include <sys/wait.h>
-# include <unistd.h>
-
-# define MAX_INPUT_SIZE 1024
-# define READ_BUFFER_SIZE 1024
-
-// Structure for the linked list node
-typedef struct s_history
-{
-	char				*command;
-	struct s_history	*next;
-}						t_history;
-
-// Function prototypes
-void					add_to_history(t_history **history, char *command);
-void					print_history(t_history *history);
-void					free_history(t_history *history);
-void					execute_command(char *command);
-char	*read_input(void); // Custom function to read input
+# include "parsing/parsing.h"
+# include "execution/execution.h"
 
 #endif

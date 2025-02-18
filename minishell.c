632bac4172211ee5533dc8921f2ec0b929496ba0
J00ZzZ <liaujoel1117@gmail.com>
2025-02-19 04:33:46 +0800

    test2:

--- minishell.c
+++ minishell.c
@@ -1,98 +1,115 @@
 #include "minishell.h"
 
 // Custom function to read input using read()
-char *read_input(void) 
+char	*read_input(void)
 {
-    char buffer[READ_BUFFER_SIZE];
-    char *input = NULL;
-    ssize_t bytes_read;
+	char	buffer[READ_BUFFER_SIZE];
+	char	*input;
+	ssize_t	bytes_read;
 
-    while ((bytes_read = read(STDIN_FILENO, buffer, READ_BUFFER_SIZE - 1)) > 0) 
-    {
-        buffer[bytes_read] = '\0'; // Null-terminate the buffer
-        input = ft_strjoin_free(input, buffer); // Concatenate and free old input
-
-        if (ft_strchr(buffer, '\n'))  // Stop reading if newline is found
-            break;
-    }
-
-    if (input && ft_strlen(input) > 0)
-        input[ft_strlen(input) - 1] = '\0'; // Remove the newline character
-    return input;
+	input = NULL;
+	while ((bytes_read = read(STDIN_FILENO, buffer, READ_BUFFER_SIZE - 1)) > 0)
+	{
+		buffer[bytes_read] = '\0'; // Null-terminate the buffer
+		input = ft_strjoin_free(input, buffer);
+		// Concatenate and free old input
+		if (ft_strchr(buffer, '\n'))
+		{ // Stop reading if newline is found
+			break ;
+		}
+	}
+	if (input && ft_strlen(input) > 0)
+	{
+		input[ft_strlen(input) - 1] = '\0'; // Remove the newline character
+	}
+	return (input);
 }
 
-
-
 // Function to add a command to the history
-void add_to_history(t_history **history, char *command) 
+void	add_to_history(t_history **history, char *command)
 {
-    t_history *new_node = (t_history *)ft_calloc(1, sizeof(t_history));
-    if (!new_node) 
-    {
-        perror("ft_calloc");
-        exit(EXIT_FAILURE);
-    }
-    new_node->command = ft_strdup(command); // Use ft_strdup from libft
-    if (!new_node->command) 
-    {
-        perror("ft_strdup");
-        free(new_node);
-        exit(EXIT_FAILURE);
-    }
-    new_node->next = NULL;
+	t_history	*new_node;
+	t_history	*current;
 
-    if (*history == NULL) 
-        *history = new_node; // If the list is empty, this is the first node
-    else 
-    {
-        t_history *current = *history;
-        while (current->next != NULL)
-            current = current->next; // Traverse to the end of the list
-        current->next = new_node; // Append the new node
-    }
+	new_node = (t_history *)ft_calloc(1, sizeof(t_history));
+	if (!new_node)
+	{
+		perror("ft_calloc");
+		exit(EXIT_FAILURE);
+	}
+	new_node->command = ft_strdup(command); // Use ft_strdup from libft
+	if (!new_node->command)
+	{
+		perror("ft_strdup");
+		free(new_node);
+		exit(EXIT_FAILURE);
+	}
+	new_node->next = NULL;
+	if (*history == NULL)
+	{
+		*history = new_node; // If the list is empty, this is the first node
+	}
+	else
+	{
+		current = *history;
+		while (current->next != NULL)
+		{
+			current = current->next; // Traverse to the end of the list
+		}
+		current->next = new_node; // Append the new node
+	}
 }
 
 // Function to print the command history
-void print_history(t_history *history) 
+void	print_history(t_history *history)
 {
-    t_history *current = history;
-    int index = 1;
-    while (current) 
-    {
-        ft_printf("%d: %s\n", index, current->command); // Use ft_printf from libft
-        current = current->next;
-        index++;
-    }
+	t_history	*current;
+	int			index;
+
+	current = history;
+	index = 1;
+	while (current)
+	{
+		ft_printf("%d: %s\n", index, current->command);
+		// Use ft_printf from libft
+		current = current->next;
+		index++;
+	}
 }
 
 // Function to free the history linked list
-void free_history(t_history *history) 
+void	free_history(t_history *history)
 {
-    t_history *tmp;
-    while (history) 
-    {
-        tmp = history;
-        history = history->next;
-        free(tmp->command); // Use ft_free from libft
-        free(tmp); // Use ft_free from libft
-    }
+	t_history	*tmp;
+
+	while (history)
+	{
+		tmp = history;
+		history = history->next;
+		free(tmp->command); // Use ft_free from libft
+		free(tmp);          // Use ft_free from libft
+	}
 }
 
 // Function to execute a command
-void execute_command(char *command) {
-    pid_t pid = fork();
-    if (pid == 0) 
-    {
-        // Child process
-        char *args[] = {"/bin/sh", "-c", command, NULL};
-        execvp(args[0], args);
-        perror("execvp");
-        exit(EXIT_FAILURE);
-    } 
-    else if (pid > 0) // Parent process
-        wait(NULL);
-    else 
-        perror("fork");
+void	execute_command(char *command)
+{
+	pid_t	pid;
+	char	*args[] = {"/bin/sh", "-c", command, NULL};
+
+	pid = fork();
+	if (pid == 0)
+	{
+		// Child process
+		execvp(args[0], args);
+		perror("execvp");
+		exit(EXIT_FAILURE);
+	}
+	else if (pid > 0)
+		// Parent process
+		wait(NULL);
+	else
+		perror("fork");
 }
 
 int main() 
--- minishell.h
+++ minishell.h
@@ -1,24 +1,25 @@
 #ifndef MINISHELL_H
-#define MINISHELL_H
+# define MINISHELL_H
 
-#include "libft/libft.h" // Include the libft header
-#include <unistd.h>
-#include <sys/wait.h>
+# include "libft/libft.h" // Include the libft header
+# include <sys/wait.h>
+# include <unistd.h>
 
-#define MAX_INPUT_SIZE 1024
-#define READ_BUFFER_SIZE 1024
+# define MAX_INPUT_SIZE 1024
+# define READ_BUFFER_SIZE 1024
 
 // Structure for the linked list node
-typedef struct s_history {
-    char *command;
-    struct s_history *next;
-} t_history;
+typedef struct s_history
+{
+	char				*command;
+	struct s_history	*next;
+}						t_history;
 
 // Function prototypes
-void add_to_history(t_history **history, char *command);
-void print_history(t_history *history);
-void free_history(t_history *history);
-void execute_command(char *command);
-char *read_input(void); // Custom function to read input
+void					add_to_history(t_history **history, char *command);
+void					print_history(t_history *history);
+void					free_history(t_history *history);
+void					execute_command(char *command);
+char	*read_input(void); // Custom function to read input
 
 #endif

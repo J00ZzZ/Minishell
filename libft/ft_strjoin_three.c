c84578726ef02ff747f4e312087e93f238d7f2e5
harleynghx <harleynghx.edu@gmail.com>
2025-03-14 17:08:05 +0800

    added libft funcrtions

---
+++ libft/ft_strjoin_three.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin_three.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: harleyng <harleyng@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/14 17:02:30 by harleyng          #+#    #+#             */
+/*   Updated: 2025/03/14 17:03:17 by harleyng         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h" // Include your libft header
+
+char	*ft_strjoin_three(char const *s1, char const *s2, char const *s3)
+{
+	char	*result;
+	size_t	len1;
+	size_t	len2;
+	size_t	len3;
+
+	if (!s1 || !s2 || !s3)
+		return (NULL);
+	len1 = ft_strlen(s1);
+	len2 = ft_strlen(s2);
+	len3 = ft_strlen(s3);
+	result = (char *)malloc(len1 + len2 + len3 + 1);
+	if (!result)
+		return (NULL);
+	ft_memcpy(result, s1, len1);
+	ft_memcpy(result + len1, s2, len2);
+	ft_memcpy(result + len1 + len2, s3, len3);
+	result[len1 + len2 + len3] = '\0';
+	return (result);
+}
